e53484d1ac68f6dcd5878a7c6d50757e91a08fc2
ZhaoZichao <draven323@matehub.top>
2026-05-20 19:28:21 +0800

    添加笔记目录

--- tina/__generated__/types.ts
+++ tina/__generated__/types.ts
@@ -615,7 +615,7 @@ export const ExperimentalGetTinaClient = () =>
   getSdk(
     generateRequester(
       createClient({
-        url: "http://localhost:4001/graphql",
+        url: "https://content.tinajs.io/2.3/content/a55e3c97-e341-48df-b0e4-ceae62e5d55a/github/main",
         queries,
       })
     )
